b434b8fa05a38acef7267db6baff3c7938e4ce68
Zamiir <oolyooolyo@gmail.com>
2024-11-28 04:29:08 +0000

    mtn comit

---
+++ src/Loop/ForLoop.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i < 15; ++i) {
+            System.out.print("\n hello world");
+        }
+
+    }
+}
